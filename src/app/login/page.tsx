00b36a687058a5e7367ab0429cfe4143ccddf499
RyoNagashiro9280 <kd1322303@st.kobedenshi.ac.jp>
2025-06-17 14:14:56 +0900

    一部コメントアウト

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,12 +23,12 @@ const Login = () => {
         <div className="flex flex-col items-center justify-center h-screen">
             <form className="w-96 p-8 bg-white rounded-lg shadow-md">
                 
-                //見出しとラベル
+                {/* //見出しとラベル */}
                 <h1 className="mb-4 text-2xl font-medium text-grey-700">ログイン</h1>
                 <div className="mb-4">
                     <label className="block text-sm font-medium text-grey-600">メールアドレス</label>
                     
-                    //入力部分
+                    {/* //入力部分 */}
                     <input
                         //registerでライブラリの管理下に登録する
                             //今回は、emailという名前を設定
@@ -51,7 +51,7 @@ const Login = () => {
                         
                         className="w-full p-2 mt-1 border-2 rounded-md"
                     />
-                    //hookでエラーがでた場合のエラー内容
+                    {/* //hookでエラーがでた場合のエラー内容 */}
                     {errors.email && (
                         <span className="text-sm text-red-600">{errors.email.message}</span>
                     )}
@@ -74,13 +74,13 @@ const Login = () => {
                     )}
                 </div>
 
-                //ログインボタン
+                {/* //ログインボタン */}
                 <div className="flex justify-end">
                     <button type="submit" className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
                         ログイン
                     </button>
                 </div>
-                //新規登録への案内リンク
+                {/* //新規登録への案内リンク */}
                 <div className="mt-4">
                     <span className="text-sm text-grey-600">初めてのご利用の方は</span>
                     <Link href="/register" className="ml-1 text-sm font-bold text-blue-500 hover:text-blue-700">
