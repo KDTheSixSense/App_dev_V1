66931b2f33f9e458193892f4622029d63cda4ebd
RyoNagashiro <kd1322303@st.kobedenshi.ac.jp>
2025-11-28 23:33:01 +0000

    ｔ

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.svg$': '<rootDir>/src/__mocks__/svg.js',
   },
 };
